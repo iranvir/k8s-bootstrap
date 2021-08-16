319f99cc3009fe000762a113a19730d44c66a5cd
Ranvir Singh <sranvir155@gmail.com>
2021-08-16 14:47:05 +0530

    Finalize master configuration. Add k8s-bootstrap tag on droplets

--- doctl-scripts/doctl-delete.sh
+++ doctl-scripts/doctl-delete.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-doctl compute droplet list --no-header | awk '{print $1}' | xargs -I vm doctl compute droplet delete vm --force
-sed -i '/#k8s-bootstrap/d' /etc/hosts
+doctl compute droplet list --tag-name k8s-bootstrap --no-header | awk '{print $1}' | xargs -I vm doctl compute droplet delete vm --force
+sed -i '/# k8s-bootstrap/d' /etc/hosts
 rm $HOME/.ssh/known_hosts
