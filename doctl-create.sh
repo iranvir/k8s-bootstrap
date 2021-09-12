8b42a9c28863890b2fbb7e6cd85a0e86accd59ec
Ranvir Singh <sranvir155@gmail.com>
2021-09-12 10:55:23 +0530

    Change sleep time from 10 to 30 seconds because reasons....

--- doctl-create.sh
+++ doctl-create.sh
@@ -3,6 +3,6 @@ hosts=$(cat ./hosts)
 for host in $hosts ; do
     doctl compute droplet create $host --image debian-10-x64 --ssh-keys c8:df:91:3a:17:dc:63:d3:77:ad:01:4e:45:07:3f:b0 --region lon1 --size s-2vcpu-2gb-amd --no-header --tag-name k8s-bootstrap 
 done
-sleep 10
+sleep 30
 doctl compute d ls --no-header | awk '{ print $3"\t"$2" # k8s-bootstrap"}' >> /etc/hosts
 exit 0
